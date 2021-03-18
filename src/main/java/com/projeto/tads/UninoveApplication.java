bf928bedbdcfbda525e2d57844153172a83042bb
Vinícius Palmeira dos Santos - VSN <vinicius.santos@iteris.local>
2021-03-18 20:43:28 -0300

    add structure

---
+++ src/main/java/com/projeto/tads/UninoveApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.tads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UninoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UninoveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/tads/UninoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.tads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UninoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
